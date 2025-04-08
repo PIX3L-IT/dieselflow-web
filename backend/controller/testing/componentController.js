c91c68fdd2643d5c7308892062bc8c19342d295b
MariaJoseGaytan <A01706616@tec.mx>
2025-04-07 22:18:56 -0600

    feat: componente modal

--- backend/controller/testing/componentController.js
+++ backend/controller/testing/componentController.js
@@ -1,6 +1,10 @@
 exports.getNavbar = (req, res) => {
     // Enviar la lista de URLs firmadas al cliente
     res.render('includes/navbar', { active: '' });
-
-
   };
+
+  exports.getModal = (req, res) => {
+    res.render('testing/modalTest');
+  };
+  
+  
