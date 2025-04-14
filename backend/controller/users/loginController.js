4d66e3afe37e21b4a0e713dc24ad2c9c4e72ee69
Zathiald <A01705403@tec.mx>
2025-04-14 09:21:20 -0600

    feat: login con username y lastName

--- backend/controller/users/loginController.js
+++ backend/controller/users/loginController.js
@@ -5,27 +5,43 @@ const Role = require('../../models/users/Role')
 
 // POST Login
 const loginUser = async (req, res) => {
-  const { email, password, tipo } = req.body;
+  const { user, password, type } = req.body;
 
   try {
-    const user = await User.findOne({ email }).populate("idRole");
-    
-    if (!user || !(await bcrypt.compare(password, user.password))) {
+    const parts = user.trim().split(" ");
+    let userLogin = null;
+
+    if (parts.length === 2) {
+      // Caso: username + lastname
+      const [username, lastName] = parts;
+      userLogin = await User.findOne({ username, lastName }).populate("idRole");
+    } else {
+      // Caso: correo o username solo
+      const esEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(user);
+      userLogin = await User.findOne(
+        esEmail ? { email: user } : { username: user }
+      ).populate("idRole");
+    }
+
+    if (!userLogin || !(await bcrypt.compare(password, userLogin.password))) {
       return res.status(401).json({ message: 'Credenciales inválidas' });
     }
 
-    if (user.idRole.roleName !== tipo) {
-      return res.status(403).json({ message: `Acceso denegado. Se requiere rol: ${tipo}` });
+    if (userLogin.idRole.roleName !== type) {
+      return res.status(403).json({
+        message:
+          `Acceso denegado. Se requiere rol: ${type}`
+      });
     }
 
     const accessToken = jwt.sign(
-      { id: user._id, email: user.email },
+      { id: userLogin._id, email: userLogin.email },
       process.env.JWT_SECRET,
       { expiresIn: process.env.JWT_EXPIRATION }
     );
 
     const refreshToken = jwt.sign(
-      { id: user._id },
+      { id: userLogin._id },
       process.env.JWT_REFRESH_SECRET,
       { expiresIn: process.env.JWT_REFRESH_EXPIRATION }
     );
@@ -44,7 +60,7 @@ const loginUser = async (req, res) => {
       secure: false
     });
 
-    res.json({ message: 'Login exitoso', rol: tipo });
+    res.json({ message: 'Login exitoso', rol: type });
 
   } catch (err) {
     console.error(err);
@@ -56,7 +72,7 @@ const loginUser = async (req, res) => {
 
 // GET Login
 const login = (req, res) => {
-  res.render('users/login', { title: 'Login' });
+  res.render('users/login');
 };
 
 module.exports = {
