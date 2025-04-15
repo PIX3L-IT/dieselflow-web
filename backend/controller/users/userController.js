0aab0105eb06015e13db2d3e08804e07ae80c849
carlosagalicia <alexsilvag82@gmail.com>
2025-04-15 17:09:56 -0600

    fix: añadir status code 200

--- backend/controller/users/userController.js
+++ backend/controller/users/userController.js
@@ -4,7 +4,7 @@ const Role = require("../../models/users/Role");
 exports.getUsers = async (req, res) => {
   try {
     const users = await User.find().populate('idRole').exec();
-    res.render('users/users', { users: users });
+    res.status(200).render('users/users', { users: users });
   } catch (error) {
     console.error("Error al obtener los usuarios:", error);
     res.status(500).render('includes/500', {
