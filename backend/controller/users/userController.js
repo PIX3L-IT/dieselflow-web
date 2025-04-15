111959962b70e9ec91b0da14012ccaa66e933948
carlosagalicia <alexsilvag82@gmail.com>
2025-04-15 14:45:32 -0600

    feat: mostrar estado de usuario

--- backend/controller/users/userController.js
+++ backend/controller/users/userController.js
@@ -3,27 +3,12 @@ const Role = require("../../models/users/Role");
 
 exports.getUsers = async (req, res) => {
   try {
-
-    const {role, search } = req.query;
-    const filter = {};
-    
-    if (search) {
-      filter['username'] = { $regex: search, $options: 'i' };
-    }
-
-    
-    const users = await User.find(filter).populate('idRole').exec();
-    
-    const filteredUsers = role
-    ? users.filter(user => user.idRole && user.idRole.roleName === role)
-    : users;  
-
-    res.render('users/users', { users: filteredUsers, role });
-
+    const users = await User.find().populate('idRole').exec();
+    res.render('users/users', { users: users });
   } catch (error) {
     console.error("Error al obtener los usuarios:", error);
-    res.status(500).render('includes/500',{
-      active: ""  
+    res.status(500).render('includes/500', {
+      active: ""
     });
   }
 }
