1f49815ac088be6ed2592c8a17594f9360fb85d3
Benjamin Arauz <benjaminarauzc@gmail.com>
2025-04-16 11:00:34 -0600

    feat: agregar la liga encriptada

--- backend/controller/users/recoveryPasswordController.js
+++ backend/controller/users/recoveryPasswordController.js
@@ -1,8 +1,8 @@
+require("dotenv").config();
 const ejs = require('ejs');
 const path = require('path');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
-const config = require('dotenv').config().parsed;
 const mailer = require('../../utils/nodemailer.js');
 const User = require("../../models/users/User");
 const Role = require("../../models/users/Role");
@@ -18,14 +18,16 @@ exports.postConfirmEmail = async (req, res, next) => {
         if (!email) {
             console.log("Se requiere un correo electrónico");
         }
-        
         const user = await User.findOne({ email }).populate('idRole').exec();
+
         if (user && user.idRole.roleName === 'Administrador') {
-            console.log("Usuario encontrado:", user);
+            const token = jwt.sign({ email: user.email }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN });
+            const resetPasswordLink = `${process.env.ENVIRONMENT_URL}/users/reset-password/?token=${token}`; 
 
             const templatePath = path.join(__dirname, '../../../frontend-web/views/users/email.ejs');
             const templateData = {
-                username: user.username
+                user: user,
+                link: resetPasswordLink,
             };
 
             const html = await ejs.renderFile(templatePath, templateData);
@@ -38,33 +40,53 @@ exports.postConfirmEmail = async (req, res, next) => {
 };
 
 exports.getResetPassword = (req, res, next) => {
-    res.render("users/resetPassword");
+    const token = req.query.token;
+    res.render("users/resetPassword", { 
+        token: token,
+    });
 };
 
 exports.postResetPassword = async (req, res, next) => {
-    const email = "benjaminarauzc@gmail.com";
+    const token = req.body.token;
     const password = req.body.password;
     const confirmPassword = req.body.confirmPassword;
-    try {
-        if (password === confirmPassword) {
-            const hashed = await bcrypt.hash(password, 10);
-            const updatedUser = await User.findOneAndUpdate(
-                { email },
-                { password: hashed },
-                { new: true }
-            );
-        
-            if (!updatedUser) {
-                return res.status(404).json({ message: 'Usuario no encontrado' });
-            }
-            res.status(200).json({ message: 'Contraseña actualizada correctamente', user: updatedUser });
+
+    // Verificar el token JWT
+    jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
+        if (err) {
+            console.error('Error al verificar el token JWT:', err);
+            // response.redirect('/auth/login');
         } else {
-            return res.status(400).json({ message: 'Las contraseñas no coinciden' });
+            // Token válido, proceder con la actualización de la contraseña
+            try {
+                if (password === confirmPassword) {
+                    const email = decoded.email;
+                    console.log("Email decodificado:", email);
+                    // Verificar si el usuario existe
+                    const user = await User.findOne({ email }).populate('idRole').exec();
+                    if (user) {
+                        const hashed = await bcrypt.hash(password, 10);
+                        const updatedUser = await User.findOneAndUpdate(
+                            { email },
+                            { password: hashed },
+                            { new: true }
+                        );
+                    
+                        if (!updatedUser) {
+                            return res.status(404).json({ message: 'Usuario no encontrado' });
+                        }
+                        res.status(200).json({ message: 'Contraseña actualizada correctamente', user: updatedUser });   
+                    } else {
+                        return res.status(404).json({ message: 'Usuario no encontrado' });
+                    }
+                } else {
+                    return res.status(400).json({ message: 'Las contraseñas no coinciden' });
+                }
+                
+            } catch (error) {
+                console.error('Error al actualizar la contraseña:', error);
+                res.status(500).json({ message: 'Error del servidor' });
+            }
         }
-        
-    } catch (error) {
-        console.error("Error al actualizar la contraseña:", error);
-        res.status(500).json({ message: 'Error del servidor' });
-    }
-    
+    });
 };
