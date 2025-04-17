777ba9f35a82a15747312996b4761c9a56776dd7
carlosagalicia <alexsilvag82@gmail.com>
2025-04-17 09:51:00 -0600

    refactor: cambiar el nombre de archivos

--- backend/routes/users/userRoutes.js
+++ backend/routes/users/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const verifyToken = require('../../utils/verifyToken');
-const { getUsers } = require("../../controller/users/userController");
+const { getUsers } = require("../../controller/users/getUsersController");
 
 router.get("/usuarios", getUsers);
 
