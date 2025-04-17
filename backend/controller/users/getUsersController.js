81f67ee77cdca55a117b9e9d212278d973d9e6e5
carlosagalicia <alexsilvag82@gmail.com>
2025-04-17 10:23:31 -0600

    fix: añadir return al controlador

--- backend/controller/users/getUsersController.js
+++ backend/controller/users/getUsersController.js
@@ -4,10 +4,10 @@ const Role = require("../../models/users/Role");
 exports.getUsers = async (req, res) => {
   try {
     const users = await User.find().populate('idRole').exec();
-    res.status(200).render('users/users', { users: users });
+    return res.status(200).render('users/users', { users: users });
   } catch (error) {
     console.error("Error al obtener los usuarios:", error);
-    res.status(500).render('includes/500', {
+    return res.status(500).render('includes/500', {
       active: ""
     });
   }
