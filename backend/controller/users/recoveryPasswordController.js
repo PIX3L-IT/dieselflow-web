491e667fdd951ff2f70cbd58977e77034905bbe0
Benjamin Arauz <benjaminarauzc@gmail.com>
2025-04-18 17:31:31 -0600

    feature: agregar mensaje de exito al cambiar contraseña

--- backend/controller/users/recoveryPasswordController.js
+++ backend/controller/users/recoveryPasswordController.js
@@ -6,6 +6,7 @@ const bcrypt = require('bcryptjs');
 const mailer = require('../../utils/nodemailer.js');
 const User = require("../../models/users/User");
 const Role = require("../../models/users/Role");
+const { error } = require("console");
 
 exports.getConfirmEmail = (req, res, next) => {
     res.render("users/confirmEmail");
@@ -13,35 +14,28 @@ exports.getConfirmEmail = (req, res, next) => {
 
 exports.postConfirmEmail = async (req, res, next) => {
     const email = req.body.email;
-    const person = req.body.person;
 
     if (email === "") {
         console.error("El correo no puede estar vacío");
-        if (person !== "Administrador"){
-            return res.status(400).json({
-                error: "Se requiere un correo electrónico",
-            });
-        }
+        return res.status(400).json({
+            error: "Se requiere un correo electrónico",
+        });
     }
 
     try {
         const user = await User.findOne({ email }).populate('idRole').exec();
         if (!user) {
             console.error("El correo electrónico no está registrado");
-            if (person !== "Administrador"){
-                return res.status(404).json({
-                    error: "No se pudo completar la solicitud",
-                });
-            }
+            return res.status(404).json({
+                error: "No se pudo completar la solicitud",
+            });
         }
 
         if (user.idRole.roleName !== 'Administrador') {
             console.error("El usuario no tiene permisos de administrador");
-            if (person !== "Administrador"){
-                return res.status(403).json({
-                    error: "No se pudo completar la solicitud",
-                });
-            }
+            return res.status(403).json({
+                error: "No se pudo completar la solicitud",
+            });
         }
 
         const token = jwt.sign(
@@ -75,8 +69,8 @@ exports.postConfirmEmail = async (req, res, next) => {
         }
     } catch (error) {
         console.error("Error en el servidor:", error);
-        return res.status(500).json({
-            error: "Error en el servidor",
+        return res.status(500).render("users/confirmEmail", {
+            error: "No se pudo completar la solicitud",
         });
     }
 };
@@ -86,15 +80,18 @@ exports.getResetPassword = (req, res, next) => {
 
     if (!token) {
         console.error('Token no proporcionado');
-        return res.redirect('/');
+        return res.render("users/login", {
+            error: "Token no proporcionado",
+        });
     }
-
     
     // Verificar el token JWT
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
         if (err) {
             console.error('Error al verificar el token JWT:', err);
-            return res.redirect('/');
+            return res.render("users/login", {
+                error: 'Token inválido o expirado',
+            });
         } else {
             return res.render("users/resetPassword", { 
                 token: token,
@@ -108,32 +105,40 @@ exports.postResetPassword = async (req, res, next) => {
     const token = req.body.token;
     const password = req.body.password;
     const confirmPassword = req.body.confirmPassword;
-    const person = req.body.person;
+    const type = req.body.type;
 
     if (password === "" || confirmPassword === "") {
-        console.error("Se requiere una contraseña");
-        return returnRenderMsg(
-            res, person, 400,
-            "Se requiere una contraseña", 
-            token, "users/resetPassword",
-        );
+        const error = "Se requiere una contraseña";
+        console.error(error);
+        if (type === "Administrador"){
+            return res.render("users/resetPassword", {
+                token: token,
+                error: error,
+            });
+        }
+        return res.status(400).json({ error });
     }
 
     if (password !== confirmPassword) {
-        console.error("Las contraseñas no coinciden");
-        return returnRenderMsg(
-            res, person, 400,
-            "Las contraseñas no coinciden", 
-            token, "users/resetPassword",
-        );
+        const error = "Las contraseñas no coinciden";
+        console.error(error);
+        if (type === "Administrador"){
+            return res.render("users/resetPassword", {
+                token: token,
+                error: error,
+            });
+        }
+        return res.status(400).json({ error });
     }
 
     // Verificar el token JWT
     jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
         if (err) {
             console.error('Error al verificar el token JWT:', err);
-            if (person === "Administrador"){
-                return res.redirect('/');
+            if (type === "Administrador"){
+                return res.render("users/login", {
+                    error: "No se pudo completar la solicitud",
+                });
             }
             return res.status(401).json({
                 error: 'Token inválido o expirado',
@@ -146,12 +151,15 @@ exports.postResetPassword = async (req, res, next) => {
                 const user = await User.findOne({ email }).populate('idRole').exec();
                 
                 if (!user) {
-                    console.error("Usuario no encontrado");
-                    return returnRenderMsg(
-                        res, person, 404,
-                        "Usuario no encontrado", 
-                        token, "users/resetPassword",
-                    );
+                    const error = "Usuario no encontrado";
+                    console.error(error);
+                    if (type === "Administrador"){
+                        return res.render("users/resetPassword", {
+                            token: token,
+                            error: error,
+                        });
+                    }
+                    return res.status(404).json({ error });
                 }
             
                 const hashed = await bcrypt.hash(password, 10);
@@ -162,20 +170,25 @@ exports.postResetPassword = async (req, res, next) => {
                 );
                 
                 if (!updatedUser) {
-                    console.error("Usuario no encontrado");
-                    return returnRenderMsg(
-                        res, person, 404,
-                        "Usuario no encontrado", 
-                        token, "users/resetPassword",
-                    );
+                    const error = "Usuario no encontrado";
+                    console.error(error);
+                    if (type === "Administrador"){
+                        return res.render("users/resetPassword", {
+                            token: token,
+                            error: error,
+                        });
+                    }
+                    return res.status(404).json({ error });
                 }
 
-                if (person === "Administrador"){
-                    return res.redirect('/');
+                const msg = "Contraseña actualizada correctamente";
+                if (type === "Administrador"){
+                    return res.render("users/login", {
+                        error: "",
+                        msg: msg,
+                    });
                 }
-                return res.status(200).json({
-                    message: "Contraseña actualizada correctamente",
-                });
+                return res.status(200).json({ msg });
 
             } catch (error) {
                 console.error('Error al actualizar la contraseña:', error);
@@ -186,16 +199,3 @@ exports.postResetPassword = async (req, res, next) => {
         }
     });
 };
-
-const returnRenderMsg = (res, person, code, error, token, url) => {
-    if (person === "Administrador"){
-        return res.render(url, {
-            token: token,
-            error: error,
-        });
-    } else {
-        return res.status(code).json({
-            error: error,
-        });
-    }
-}
